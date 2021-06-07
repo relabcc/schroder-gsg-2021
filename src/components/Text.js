b738692604016e6fd81972237d7453055b1fe68f
Peja <peja@relab.cc>
2021-06-07 11:52:30 +0800

    updated fonsize

--- src/components/Text.js
+++ src/components/Text.js
@@ -36,9 +36,8 @@ Text.defaultProps = {
 Text.inline = (props) => <Text is="span" {...props} />;
 Text.bold = (props) => <Text fontWeight="bold" {...props} />;
 Text.thin = (props) => <Text fontWeight="200" {...props} />;
-Text.Desc = (props) => <Text fontWeight="500" fontSize={responsive('2rem', '2.8rem')} {...props} />;
-Text.SmallDesc = (props) => <Text fontSize={responsive('1.8rem', '2.4rem')} {...props} />;
-Text.Title = forwardRef((props, ref) => <Text ref={ref} fontWeight="900" color="titleBlue" fontSize={responsive('2.6rem', '4rem')} {...props} />)
+Text.Desc = (props) => <Text fontWeight="500" fontSize={responsive('1.8rem', '2.4rem')} {...props} />; 
+Text.Title = forwardRef((props, ref) => <Text ref={ref} fontWeight="900" color="titleBlue" fontSize={responsive('2.4rem', '3.6rem')} {...props} />)
 Text.SubTitle = forwardRef((props, ref) => <Text.Title ref={ref} fontWeight="bold" color="darkGreen" fontSize={responsive('2rem', '2.8rem')} {...props} />)
 Text.ModuleTitle = forwardRef((props, ref) => <Text.Title ref={ref} fontWeight="bold" {...props} />)
 Text.SmallTitle = forwardRef((props, ref) => <Text ref={ref} fontWeight="bold" fontSize={responsive('1.8rem', '2.4rem')} {...props} />)

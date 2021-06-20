6eb0f340e78e6f898276f1a550373af8bdd272a8
Renddi Yeh <renddi@relab.cc>
2021-06-20 19:04:23 +0800

    updated btn track

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import HomePage from './containers/HomePage'
 
 function App() {
+  useEffect(() => {
+    document.body.append(window.__btnBackToTop)
+    window.__tabButtons.forEach(ele => document.body.append(ele))
+  }, [])
   return (
     <HomePage />
   );

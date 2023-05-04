0f8e1e2a50f47fe079b9f4d1a57a2f06c3599bbd
Renddi Yeh <renddi@relab.cc>
2023-05-04 22:28:16 +0800

    updated

--- src/index.js
+++ src/index.js
@@ -1,46 +1,39 @@
-import 'react-app-polyfill/ie11'
-import 'react-app-polyfill/stable'
-import 'proxy-polyfill'
-import 'abortcontroller-polyfill'
-import smoothscroll from 'smoothscroll-polyfill';
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import "proxy-polyfill";
+import "abortcontroller-polyfill";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import find from 'lodash/find'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import ThemeProvider from './components/ThemeProvider'
+import ThemeProvider from "./components/ThemeProvider";
 
-import MediaProvider from './contexts/mediaQuery/MediaProvider'
-import DataProvider from './contexts/data/DataProvider'
-import App from './App';
-
-smoothscroll.polyfill();
-
-window.injectCSS = cssLink => {
-  const tag = document.createElement('link')
-  tag.href = cssLink
-  tag.rel = 'stylesheet'
-  document.head.appendChild(tag)
-}
+import MediaProvider from "./contexts/mediaQuery/MediaProvider";
+import DataProvider from "./contexts/data/DataProvider";
+import App from "./App";
 
 const getContianer = () => {
-  const target = document.getElementById('gsg2021-tw-root');
-  window.__video = target.querySelector('[data-bynder-widget]')
+  const target = document.getElementById("gsg2021-tw-root");
+  window.__video = target.querySelector("[data-bynder-widget]");
   // window.__btnBackToTop = target.querySelector('#backToTop')
   // window.__tabButtons = Array.from(target.querySelectorAll('.btn-carousel'))
 
-  target.style.padding = '0';
+  target.style.padding = "0";
   return target;
 };
 
 setTimeout(() => {
-  ReactDOM.render(
-    <ThemeProvider>
-      <MediaProvider>
-        <DataProvider>
-          <App />
-        </DataProvider>
-      </MediaProvider>
-    </ThemeProvider>
-  , getContianer());
-})
+  if (!window._RE_INITED_) {
+    ReactDOM.render(
+      <ThemeProvider>
+        <MediaProvider>
+          <DataProvider>
+            <App />
+          </DataProvider>
+        </MediaProvider>
+      </ThemeProvider>,
+      getContianer()
+    );
+    window._RE_INITED_ = true;
+  }
+});

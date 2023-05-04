669a7286a6d0e84041c88721f615de96f028a6f4
Renddi Yeh <renddi@relab.cc>
2023-05-04 20:26:28 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, { useEffect } from 'react';
 import HomePage from './containers/HomePage'
 
 function App() {
-  useEffect(() => {
-    document.body.append(window.__btnBackToTop)
-    window.__tabButtons.forEach(ele => document.body.append(ele))
-  }, [])
+  // useEffect(() => {
+  //   document.body.append(window.__btnBackToTop)
+  //   window.__tabButtons.forEach(ele => document.body.append(ele))
+  // }, [])
   return (
     <HomePage />
   );
--- src/index.js
+++ src/index.js
@@ -26,15 +26,11 @@ window.injectCSS = cssLink => {
 const getContianer = () => {
   const target = document.getElementById('gsg2021-tw-root');
   window.__video = target.querySelector('[data-bynder-widget]')
-  window.__btnBackToTop = target.querySelector('#backToTop')
-  window.__tabButtons = Array.from(target.querySelectorAll('.btn-carousel'))
-  const found = find(document.querySelectorAll('.jumbotron'), (ele) => ele.contains(target));
-  if (found.classList.contains('panel-white')) {
-    found.classList.remove('panel-white')
-  }
-  const container = found || target;
-  container.style.padding = '0';
-  return container;
+  // window.__btnBackToTop = target.querySelector('#backToTop')
+  // window.__tabButtons = Array.from(target.querySelectorAll('.btn-carousel'))
+
+  target.style.padding = '0';
+  return target;
 };
 
 setTimeout(() => {

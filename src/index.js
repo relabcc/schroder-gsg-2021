ad81106093ea40bb39d351cb2917a9a4fcf280f0
Renddi Yeh <renddi@relab.cc>
2024-03-11 23:17:11 +0800

    feat: 利用MutationObserver來偵測rwd rerender

--- src/index.js
+++ src/index.js
@@ -12,56 +12,66 @@ import MediaProvider from "./contexts/mediaQuery/MediaProvider";
 import DataProvider from "./contexts/data/DataProvider";
 import HomePage from "./containers/HomePage";
 
-const mount = () => {
+const key = "gsg2021-tw-root";
+
+let mount;
+
+const attachObserver = (element) => {
+  if (element.parentElement && element.parentElement !== document.body) {
+    const callback = (mutationList, observer) => {
+      for (const mutation of mutationList) {
+        // console.log(mutation);
+        if (mutation.type === "childList") {
+          const found = Array.from(mutation.addedNodes).find((node) =>
+            node.innerHTML.includes(key)
+          );
+          if (found) {
+            // console.log("container added");
+            mount();
+          }
+        }
+      }
+    };
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+    const config = { childList: true };
+
+    // Start observing the target node for configured mutations
+    observer.observe(element.parentElement, config);
+    attachObserver(element.parentElement);
+  }
+};
+
+mount = (init) => {
+  const render = (el, index) => {
+    ReactDOM.render(
+      <ThemeProvider>
+        <MediaProvider>
+          <DataProvider>
+            <HomePage section={1 + index} />
+          </DataProvider>
+        </MediaProvider>
+      </ThemeProvider>,
+      el,
+      () => {
+        if (index === 0 && init) {
+          attachObserver(el, render);
+        }
+      }
+    );
+  };
   setTimeout(() => {
-    document.querySelectorAll(".gsg2021-tw-root").forEach((el, index) => {
+    document.querySelectorAll(`.${key}`).forEach((el, index) => {
       if (index > 3) return;
-      ReactDOM.render(
-        <ThemeProvider>
-          <MediaProvider>
-            <DataProvider>
-              <HomePage section={1 + index} />
-            </DataProvider>
-          </MediaProvider>
-        </ThemeProvider>,
-        el
-      );
+      render(el, index);
     });
   });
 };
 
 if (process.env.NODE_ENV === "development") {
   setTimeout(() => {
-    mount();
+    mount(true);
   }, 1000);
-
-  // const attachObserver = (element) => {
-  //   if (element.parentElement && element.parentElement !== document.body) {
-  //     const callback = (mutationList, observer) => {
-  //       for (const mutation of mutationList) {
-  //         console.log(mutation);
-  //         if (mutation.type === "childList") {
-  //           if (
-  //             Array.from(mutation.addedNodes).some((node) =>
-  //               node.innerHTML.includes("gsg2021-tw-root")
-  //             )
-  //           ) {
-  //             console.log("container added");
-  //             mount();
-  //           }
-  //         }
-  //       }
-  //     };
-  //     // Create an observer instance linked to the callback function
-  //     const observer = new MutationObserver(callback);
-  //     const config = { childList: true };
-
-  //     // Start observing the target node for configured mutations
-  //     observer.observe(element.parentElement, config);
-  //     attachObserver(element.parentElement);
-  //   }
-  // };
-  // attachObserver(container);
 } else {
-  mount();
+  mount(true);
 }
